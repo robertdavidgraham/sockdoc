6bc88efda1560c62b9288ef68f6aeb1949e422a3
Robert David Graham <robert_david_graham@yahoo.com>
2019-05-14 19:50:30 -0400

    mmap

--- src/util-mktime64.h
+++ src/util-mktime64.h
@@ -8,7 +8,7 @@
  * but more like 'timegm()' using UTC. This is intended for
  * use with code that must also compile on 32-bit platforms,
  * where a custom 64-bit time type is used to handle
- * the 2038 epochalypse.
+ * the 2038.
  */
 long long 
 util_mktime64(long year0, unsigned mon0,
